78b3cec2cb34792e7d175f176f94123e34165f07
Axel Magnuson <axelmagn@gmail.com>
2025-08-25 22:52:07 -0700

    write key storage for in-memory graph example

--- src/memdb.zig
+++ src/memdb.zig
@@ -2,51 +2,126 @@
 const std = @import("std");
 const array_list = std.array_list;
 const mem = std.mem;
-const Allocator = std.mem.Allocator;
 
-const Id = u64;
+const Id = u32;
 
 const Key = struct {
     space: KeySpace,
-    buf: []u8,
+    buf: []const u8,
 
-    pub fn deinit(self: *Key, gpa: mem.Allocator) void {
+    pub fn deinit(self: Key, gpa: mem.Allocator) void {
         gpa.free(self.buf);
     }
 
-    pub fn dupe(gpa: mem.Allocator, key: Key) !Key {
+    pub fn dupe(key: Key, gpa: mem.Allocator) !Key {
         return Key{
             .space = key.space,
-            .buf = try gpa.dupe(key.buf),
+            .buf = try gpa.dupe(u8, key.buf),
         };
+    }
+
+    pub fn eql(self: Key, other: Key) bool {
+        return self.space == other.space and mem.eql(u8, self.buf, other.buf);
     }
 };
 
 const KeySpace = u16;
 
 const Graph = struct {
-    gpa: mem.Allocator,
-    next_id: Id = 1, // 0 is nil
+    keys: Keys,
 
-    keys: KeyList,
+    pub const Keys = struct {
+        gpa: mem.Allocator,
+        data: KeyList,
+        next_id: Id = 1, // 0 is nil
 
-    pub const KeyEntry = struct {
-        id: Id,
-        key: Key,
+        pub const KeyEntry = struct {
+            id: Id,
+            key: Key,
+        };
+        pub const KeyList = std.ArrayList(KeyEntry);
+
+        pub fn init(gpa: mem.Allocator) Keys {
+            return Keys{
+                .gpa = gpa,
+                .data = KeyList.empty,
+            };
+        }
+
+        pub fn deinit(self: *Keys) void {
+            for (self.data.items) |entry| {
+                entry.key.deinit(self.gpa);
+            }
+            self.data.deinit(self.gpa);
+        }
+
+        /// Add a key to the index, and assign it an ID.  If they key has already been added, dedupe it.
+        pub fn intern(self: *Keys, key: Key) !Id {
+            const inner_key = try key.dupe(self.gpa);
+            // dedupe
+            if (self.lookupByKey(key)) |id| return id;
+            // add new key entry
+            const entry = KeyEntry{
+                .id = self.next_id,
+                .key = inner_key,
+            };
+            try self.data.append(self.gpa, entry);
+            defer self.next_id += 1;
+            return self.next_id;
+        }
+
+        /// remove a key from the index.  Asserts that ID assigned to a key.
+        pub fn remove(self: *Keys, id: Id) void {
+            for (0.., self.data.items) |i, entry| {
+                if (entry.id == id) {
+                    _ = self.data.swapRemove(i);
+                    entry.key.deinit(self.gpa);
+                    return;
+                }
+            }
+            unreachable;
+        }
+
+        /// look up the ID of a key in the graph.  Return null if not found.
+        pub fn lookupByKey(self: *Keys, key: Key) ?Id {
+            for (self.data.items) |entry| {
+                if (key.eql(entry.key)) return entry.id;
+            }
+            return null;
+        }
+
+        /// look up the key of an ID in the graph.  Return null if not found.
+        pub fn lookupById(self: *Keys, id: Id) ?Key {
+            for (self.data.items) |entry| {
+                if (id == entry.id) return entry.key;
+            }
+            return null;
+        }
     };
-    pub const KeyList = array_list.ArrayList(KeyEntry, null);
 
     pub fn init(gpa: mem.Allocator) Graph {
         return Graph{
-            .gpa = gpa,
-            .keys = KeyList.empty,
+            .keys = Keys.init(gpa),
         };
     }
 
     pub fn deinit(self: *Graph) void {
-        for (self.keys.items) |key| {
-            key.deinit(self.gpa);
-        }
-        self.keys.deinit(self.gpa);
+        self.keys.deinit();
     }
 };
+
+test "key storage" {
+    const t = std.testing;
+
+    var k = Graph.Keys.init(t.allocator);
+    defer k.deinit();
+
+    const alice_key = Key{ .space = 0, .buf = "Alice" };
+    const alice_id = try k.intern(alice_key);
+    try t.expectEqual(1, alice_id);
+    try t.expectEqual(1, k.lookupByKey(alice_key));
+    try t.expect(alice_key.eql(k.lookupById(alice_id).?));
+    k.remove(alice_id);
+    try t.expectEqual(null, k.lookupByKey(alice_key));
+    try t.expectEqual(null, k.lookupById(alice_id));
+}
